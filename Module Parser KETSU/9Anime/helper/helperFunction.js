160ac2c5c27ea9a5411bb90af006a5360d2f580d
mprotmod <81228102+mprotmod@users.noreply.github.com>
2021-05-02 01:24:30 +0200

    Add files via upload

---
+++ Module Parser KETSU/9Anime/helper/helperFunction.js
@@ -0,0 +1,52 @@
+function Info(request, extra, javascriptConfig) {
+    this.request = request;
+    this.extra = extra;
+    this.javascriptConfig = javascriptConfig;
+}
+
+function ModuleRequest(url, method, headers, httpBody) {
+    this.url = url;
+    this.method = method;
+    this.headers = headers;
+    this.httpBody = httpBody;
+}
+
+function Extra(commands, extraInfo) {
+    this.commands = commands;
+    this.extraInfo = extraInfo;
+}
+
+function Commands(commandName, params) {
+    this.commandName = commandName;
+    this.params = params;
+}
+
+function JavascriptConfig(removeJavascript, loadInWebView, javaScript) {
+    this.removeJavascript = removeJavascript;
+    this.loadInWebView = loadInWebView;
+    this.javaScript = javaScript;
+}
+
+function KeyValue(key, value) {
+    this.key = key;
+    this.value = value;
+}
+
+const savedData = document.getElementById('ketsu-final-data');
+const parsedJson = JSON.parse(savedData.innerHTML);
+
+let emptyKeyValue = [new KeyValue('', '')];
+
+const eps = document.querySelector('#episodes').innerHTML;
+const data = encodeURIComponent(eps.replace(/\\r?\\n|\\r/g, ''));
+
+const commands = [
+    new Commands('variable', [
+        new KeyValue('data', data),
+        new KeyValue('current', parsedJson.request.url)
+    ])
+];
+
+const infoPageObject = new Info(new ModuleRequest('', '', emptyKeyValue, null), new Extra(commands, emptyKeyValue), new JavascriptConfig(false, false, ''));
+const finalJson = JSON.stringify(infoPageObject);
+savedData.innerHTML = finalJson;
